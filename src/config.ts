13625fe127b505c85a3e21656e0415b1bfa58e5b
Lgw835 <liguowang835@gmail.com>
2025-02-04 11:54:54 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -6,7 +6,7 @@ export let PROXY_CODE_SECRET = 'Easy-Reverse-Proxy'; // 代理码密钥
 // 代理
 export let PROXIES: Proxy[] = [
     {
-        domain: "google.liguowang.online",
+        domain: "easy-reverse-proxy-ebon.vercel.app/",
         url: "https://www.google.com/",
         enable: true
     }
